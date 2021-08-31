4bf8ba18b092c1464759b5764f8779bcbdc76e17
catnap421 <wlq7410@naver.com>
2021-08-31 12:53:37 +0900

    Chore: Setting

---
+++ src/main/java/com/bookstudy/backbom/BackbomApplication.java
@@ -0,0 +1,13 @@
+package com.bookstudy.backbom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackbomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackbomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookstudy/backbom/BackbomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstudy.backbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackbomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
